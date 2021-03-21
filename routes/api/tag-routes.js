82e0b787126361eeb98c087f3c41848736a7c40e
Prestongeyer713 <75324665+Prestongeyer713@users.noreply.github.com>
2021-03-21 18:43:42 -0400

    Update tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -57,13 +57,6 @@ router.put('/:id', (req, res) => {
       id: req.params.id
     }
   })
-    .then(dbTagData => {
-      if (!dbTagData){
-        res.status(404).json({message:'No tag found with this id'});
-        return;
-      }
-      res.json(dbTagData);
-    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
